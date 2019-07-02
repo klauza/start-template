fc11db133eb3f616f7d09a42a55c3db59cf5d64f
klauza <klauza.uk@gmail.com>
2019-07-02 21:39:59 +0100

    package, filesystem update, jquery inline add

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import 'core-js/stable'; // polyfill only stable `core-js` features - ES and web standards:
 import "regenerator-runtime/runtime";
+
+
 /*   */
 import "./style.scss";
-import { app } from "./js/app.js"; 
+import app from "./js/app.js"; 
 
-Promise.resolve(2020).then(x => console.log(x)); // test
 
 app();
-
-// avatar from github api
-async function getAvatar(){
-  const response = await fetch('https://api.github.com/users/klauza');
-
-  const data = await response.json();
- 
-  return data
-}
-
-getAvatar().then(result => {
-  const avatar = result.avatar_url;
-  // console.log(avatar); 
-  document.getElementById('image').setAttribute('src', avatar);
-});
-
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,26 @@
-export const app = () => {
+const app = () => {
 
-  console.log('Hello from the console!');
+  Promise.resolve(2020).then(x => console.log(x)); // polyfill es5 test
+
+  console.log($.now()); // jquery test
+
+
+  // avatar from github api
+  async function getAvatar(){
+    const response = await fetch('https://api.github.com/users/klauza');
+
+    const data = await response.json();
+  
+    return data
+  }
+
+  getAvatar().then(result => {
+    const avatar = result.avatar_url;
+    // console.log(avatar); 
+    document.getElementById('image').setAttribute('src', avatar);
+  });
 
 }
+
+
+export default app;
